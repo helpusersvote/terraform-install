411a58a0ae42ac26a30021ed984dc283b0ff3e27
Dan Gillespie <dan@dg.gg>
2018-08-31 12:54:57 -0700

    Load template variables from ConfigMap

--- scripts/configmap.sh
+++ scripts/configmap.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Run kubectl configured to convert and not use API server.
+KCONVERT="kubectl --server 127.0.0.8 convert --local"
+
 # Reads the contents of a ConfigMap as string:string JSON map suitable for Terraform.
 function main() {
 	local filename=${1}
@@ -7,16 +10,48 @@ function main() {
 		echo "A filename containing a ConfigMap must be specified" >&2 && exit 1
 	fi
 
-	read_configmap ${filename}
+	local key=${2}
+	if [ -z ${key} ]; then
+		print_configmap ${filename} | close_json
+	else
+		read_configmap_value ${filename} ${key} | print_first_configmap - | close_json
+	fi
+
 }
 
 # Use kubectl to extract keys + values of ConfigMap.
-function read_configmap() {
+function print_configmap() {
 	local filename=${1}
-	local template=
 
-	local contents="$(kubectl --server 127.0.0.8 convert -f ${filename} -o go-template='{{range $k, $v := .data}}"{{$k}}":"{{$v}}",{{end}}ENDMAP' | grep ",ENDMAP" | sed -e "s/,ENDMAP//g")"
-	echo "{${contents}}"
+	${KCONVERT} -f ${filename} -o go-template='{{range $k, $v := .data}},"{{js $k}}":"{{js $v}}"{{end}}'
+}
+
+# Use kubectl to extract keys + values of first ConfigMap in list of them.
+function print_first_configmap() {
+	local filename=${1}
+
+	${KCONVERT} -f ${filename} -o go-template='{{range $lk, $lv := .items}}{{range $k, $v := $lv.data}},"{{js $k}}":"{{js $v}}"{{end}}{{end}}'
+}
+
+# Convert YAML formatted ConfigMap Value to temporary string:string ConfigMap for given file and key.
+function read_configmap_value() {
+	local filename=${1}
+	local key=${2}
+
+	cat <<EOF
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: temp
+data:
+  _name: ${key}
+$(${KCONVERT} -f ${filename} -o go-template="{{range \$k, \$v := .data}}{{if eq \$k \"${key}\"}}{{\$v}}{{end}}{{end}}" | sed "s/^/  /g")
+EOF
+}
+
+# Places brackets around JSON body
+function close_json() {
+	echo "{$(cat)}" | sed 's/{,/{/g'
 }
 
 main "$@"
