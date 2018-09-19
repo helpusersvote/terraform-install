6334159daaec82e686c6eef0975247b04fbcd0e6
Dan Gillespie <dan@dg.gg>
2018-09-19 12:14:08 -0700

    : setup GCP APIs

--- modules/cloudsql/main.tf
+++ modules/cloudsql/main.tf
@@ -1,11 +1,36 @@
+// Enable necessary APIs
+resource "google_project_service" "cloud_sql" {
+  service            = "sql-component.googleapis.com"
+  disable_on_destroy = "false"
+}
+
+resource "google_project_service" "cloud_sql_admin" {
+  service            = "sqladmin.googleapis.com"
+  disable_on_destroy = "false"
+}
+
+resource "google_project_service" "iam" {
+  service            = "iam.googleapis.com"
+  disable_on_destroy = "false"
+}
+
+resource "google_project_service" "resourcemanager" {
+  service            = "cloudresourcemanager.googleapis.com"
+  disable_on_destroy = "false"
+}
+
 resource "google_service_account" "sql_access" {
   account_id   = "${var.client_service_account}"
   display_name = "Allows access to Help Users Vote SQL"
+
+  depends_on = ["google_project_service.iam"]
 }
 
 resource "google_project_iam_member" "sql_access_rights" {
   role   = "roles/cloudsql.client"
   member = "serviceAccount:${google_service_account.sql_access.email}"
+
+  depends_on = ["google_project_service.resourcemanager"]
 }
 
 resource "google_sql_database_instance" "master" {
@@ -16,4 +41,9 @@ resource "google_sql_database_instance" "master" {
   settings {
     tier = "db-f1-micro"
   }
+
+  depends_on = [
+    "google_project_service.cloud_sql",
+    "google_project_service.cloud_sql_admin",
+  ]
 }
