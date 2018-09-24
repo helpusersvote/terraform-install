d359c8cc4ecf38cca7b601d245da5d9ff36b01d8
Fouad Matin <m@fouad.org>
2018-09-24 13:43:16 -0700

    fix(argo): manifest_dirs

--- modules/argo_tunnel/main.tf
+++ modules/argo_tunnel/main.tf
@@ -21,7 +21,7 @@ module "config" {
 module "kubernetes" {
   source = "../kubernetes"
 
-  manifest_dirs = "${concat(module.config.dirs, [var.certs])}"
+  manifest_dirs = "${concat(module.config.dirs, var.certs)}"
   kubeconfig    = "${var.kubeconfig}"
   do_destroy    = "${var.do_destroy}"
   last_resource = "${join(",", module.config.manifest_state)}"
