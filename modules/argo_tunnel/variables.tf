21484ca2dae2672d3317ea2b3752c81aa2836ca9
Fouad Matin <m@fouad.org>
2018-09-24 14:22:58 -0700

    fix(argo): certs default

--- modules/argo_tunnel/variables.tf
+++ modules/argo_tunnel/variables.tf
@@ -24,7 +24,7 @@ variable "kubeconfig" {
 variable "certs" {
   description = "Path to directory containing CloudFlare Argo certificates formated as Secrets"
   type        = "string"
-  default     = "jake"
+  default     = ""
 }
 
 variable "last_resource" {
