bc8ab129ec0c9f34256622a3a4b24441fe1dd6fb
Dan Gillespie <dan@dg.gg>
2018-10-09 19:24:43 -0700

    change(contour): use contour for ingress and update components to use it

---
+++ modules/contour/main.tf
@@ -0,0 +1,28 @@
+// defaults requiring interpolation
+locals {
+  render_dir    = "${var.render_dir=="" ? local.default_render_dir : var.render_dir}"
+  config_path   = "${var.config_path=="" ? local.default_config_path : var.config_path}"
+  manifests_dir = "${var.manifests_dir=="" ? local.default_manifests_dir : var.manifests_dir}"
+
+  default_render_dir    = "${path.module}/dist/manifests"
+  default_config_path   = "${path.module}/manifests/config.yaml"
+  default_manifests_dir = "${path.module}/manifests"
+}
+
+module "config" {
+  source = "../config"
+
+  components   = ["ingress"]
+  render_dir   = "${local.render_dir}"
+  config       = "${local.config_path}"
+  manifest_dir = "${local.manifests_dir}"
+}
+
+module "kubernetes" {
+  source = "../kubernetes"
+
+  manifest_dirs = "${compact(concat(module.config.dirs, list(var.certs)))}"
+  kubeconfig    = "${var.kubeconfig}"
+  do_destroy    = "${var.do_destroy}"
+  last_resource = "${join(",", module.config.manifest_state)}"
+}
---
+++ modules/contour/variables.tf
@@ -0,0 +1,39 @@
+variable "render_dir" {
+  description = "Path to directory where templated manifests can be outputted (defaults to path within module)"
+  type        = "string"
+  default     = ""
+}
+
+variable "config_path" {
+  description = "Path to ConfigMap describing configuration for config-api (defaults to path within module)"
+  type        = "string"
+  default     = ""
+}
+
+variable "manifests_dir" {
+  description = "Directory containing manifests used for deployment (defaults to path within module)"
+  type        = "string"
+  default     = ""
+}
+
+variable "kubeconfig" {
+  description = "Path to kubeconfig used to authenticate with Kubernetes API server"
+  type        = "string"
+}
+
+variable "certs" {
+  description = "Path to directory containing Ingress formatted TLS Secrets"
+  type        = "string"
+  default     = ""
+}
+
+variable "last_resource" {
+  description = "Allows dependency to be expressed to module"
+  type        = "string"
+  default     = ""
+}
+
+variable "do_destroy" {
+  description = "Actually destroy manifests, otherwise will skip"
+  default     = false
+}
